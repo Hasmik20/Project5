f4e0b96c6cc28cc3ff405b98a41c5fb97fe3a395
Hasmik20 <h_aghayan@yahoo.com>
2020-05-08 15:08:31 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('#anchor-tag').lightGallery({
+    mode: 'lg-fade',
+    thumbnail:true
+});
+
+$(document).ready(function(){
+    let images = document.getElementsByTagName('a');
+    $('#myInput').on('keyup', function(){
+        let search =$('#myInput').val().toLowerCase();
+        for(let i = 0; i < images.length; i++) {
+            let searchVal = images[i].getAttribute('data-title','data-alt');
+            if(searchVal.toLowerCase().indexOf(search) > -1){
+                images[i].style.display = "";
+            }else{
+                images[i].style.display = "none";
+            }
+        }
+    })
+})
+
+
